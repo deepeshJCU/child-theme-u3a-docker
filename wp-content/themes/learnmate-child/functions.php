b600bdd29cb31c7e80a7cec80dd6503a76fd8500
deepeshJCU <deepeshbijarnia2911@gmail.com>
2025-05-16 16:14:01 +0100

    Add dark mode toggle with icon (moon/sun) and localStorage support

---
+++ wp-content/themes/learnmate-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+add_action('wp_enqueue_scripts', 'learnmate_child_enqueue_assets');
+
+function learnmate_child_enqueue_assets() {
+    // Enqueue parent and child styles
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_uri(), array('parent-style'));
+
+    // Enqueue dark mode JS
+    wp_enqueue_script(
+        'dark-mode-toggle',
+        get_stylesheet_directory_uri() . '/js/dark-mode-toggle.js',
+        array(),
+        null,
+        true
+    );
+}
+
