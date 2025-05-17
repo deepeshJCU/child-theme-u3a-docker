bac11029db7fda8b85479d15553e145567cc6969
deepeshJCU <deepeshbijarnia2911@gmail.com>
2025-05-17 10:53:45 +0100

    Updating the Function for the style path

--- wp-content/themes/learnmate-child/functions.php
+++ wp-content/themes/learnmate-child/functions.php
@@ -4,7 +4,7 @@ add_action('wp_enqueue_scripts', 'learnmate_child_enqueue_assets');
 function learnmate_child_enqueue_assets() {
     // Enqueue parent and child styles
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-style', get_stylesheet_uri(), array('parent-style'));
+    wp_enqueue_style('child-style', get_stylesheet_uri(), array('parent-style'), wp_get_theme()->get('Version'));
 
     // Enqueue dark mode JS
     wp_enqueue_script(
@@ -15,4 +15,3 @@ function learnmate_child_enqueue_assets() {
         true
     );
 }
-
