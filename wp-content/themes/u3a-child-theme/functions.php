a94f688cd7a26c19a4d23d1c4b14cd621a97a824
deepeshJCU <deepeshbijarnia2911@gmail.com>
2025-05-19 09:31:50 +0100

    Updating the functions for main menu and footer menu

--- wp-content/themes/u3a-child-theme/functions.php
+++ wp-content/themes/u3a-child-theme/functions.php
@@ -1,14 +1,20 @@
 <?php
+// Enqueue parent and child theme styles
 function u3a_enqueue_styles() {
-    wp_enqueue_style(
-        'u3a-parent-style',
-        get_template_directory_uri() . '/style.css'
-    );
-    wp_enqueue_style(
-        'u3a-child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('u3a-parent-style'),
-        wp_get_theme()->get('Version')
-    );
+    // Load parent style
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+    // Load child style (after parent)
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
 add_action('wp_enqueue_scripts', 'u3a_enqueue_styles');
+
+// Register navigation menus
+function u3a_register_menus() {
+    register_nav_menus(array(
+        'main-menu'   => __('Main Menu', 'u3a-child-theme'),
+        'footer-menu' => __('Footer Menu', 'u3a-child-theme'),
+    ));
+}
+add_action('after_setup_theme', 'u3a_register_menus');
+
